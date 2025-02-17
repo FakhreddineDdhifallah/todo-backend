feb584f92eb7f9a9e18173f2870dd6147c1bf592
Fakhreddine Dhifallah <118848417+FakhreddineDdhifallah@users.noreply.github.com>
2025-02-17 10:16:19 +0100

    mongodb .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/todo-app'),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/nest'),
     TasksModule,
-    AuthModule, // MongoDB connection
+    AuthModule,
   ],
+  controllers: [AppController], // Make sure AppController is here!
+  providers: [AppService],
 })
 export class AppModule {}
